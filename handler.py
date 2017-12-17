7544b70d06fa1730b42ace17c00e2259811c1b73
Siebrand Mazeland <siebrand@kitano.nl>
2017-12-17 23:45:26 +0100

    Update formatting

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
+import datetime
 import json
 import os
 import sys
-import datetime
 
 here = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(here, "./vendored"))
@@ -16,7 +16,7 @@ def hello(event, context):
     try:
         data = json.loads(event["body"])
 
-        #print (data)
+        # print (data)
 
         message = str(data["message"]["text"])
         chat_id = data["message"]["chat"]["id"]
@@ -28,10 +28,13 @@ def hello(event, context):
             response = "Hallo {}! Typ '/omloop' als je wilt weten wanneer het seizoen weer begint! ".format(first_name)
 
         if "omloop" in message:
-            omloop = datetime.date(2018,2,28)
+            omloop = datetime.date(2018, 2, 28)
             response = "Omloop is over {} dagen!".format((omloop - datetime.date.today()).days)
 
-        data = {"text": response.encode("utf8"), "chat_id": chat_id}
+        data = {
+            "text": response.encode("utf8"),
+            "chat_id": chat_id
+        }
         url = BASE_URL + "/sendMessage"
         requests.post(url, data)
 
